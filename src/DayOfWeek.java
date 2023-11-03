08b150ac3ec4472c7cc5d96081e586ea615262f2
Norayr <asoyannorayr99@gmail.com>
2023-11-03 18:42:17 +0300

    add commit

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -1,23 +1,15 @@
 public class DayOfWeek {
-        public static void main(String[] args) {
-            boolean result = isWeekend("Thursday");
-            System.out.println(result);
-
-        }
-        public static boolean isWeekend(String weekday) {
-            if (weekday == "Monday") {
-                return false;
-            } else if (weekday == "Tuesday") {
-                return false;
-            } else if (weekday == "Wednesday") {
-                return false;
-            } else if (weekday == "Thursday") {
-                return false;
-            } else if (weekday == "Friday") {
-                return false;
-            } else if (weekday == "Saturday") {
-                return true;
-            } else return weekday == "Sunday";
-        }
-
+    public static void main(String[] args) {
+        System.out.println(isWeekend("Wednesday"));
     }
+
+    public static boolean isWeekend(String weekday) {
+        String[] weekends = {"Saturday", "Sunday"};
+        for (String weekend : weekends) {
+            if (weekend.equals (weekday)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
