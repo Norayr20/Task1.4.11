ead0cb24c06f80ef958f027ad9d3f0c9146444fe
Norayr <asoyannorayr99@gmail.com>
2023-11-11 18:01:39 +0300

    add commit

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -4,12 +4,6 @@ public class DayOfWeek {
     }
 
     public static boolean isWeekend(String weekday) {
-        String[] weekends = {"Saturday", "Sunday"};
-        for (String weekend : weekends) {
-            if (weekend.equals (weekday)) {
-                return true;
-            }
-        }
-        return false;
+        return weekday.equals("Saturday") || weekday.equals("Sunday");
     }
 }
